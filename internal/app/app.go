2c3827dc86a6c4e90f5d1a5cef318615279f7ddb
Mateusz <mateusz.skiba114@gmail.com>
2026-03-18 15:49:56 +0100

    fix: make app fields exported

--- internal/app/app.go
+++ internal/app/app.go
@@ -11,41 +11,47 @@ import (
 	"github.com/skiba-mateusz/RocketV2/pkg/logger"
 )
 
-type App struct {
-	logger    *logger.Logger
-	config    *config.Config
-	parser    parser.Parser
-	templater templater.Templater
-	builder   builder.Builder
-	server	  server.Server
-}
-
-func New(verbose bool) (*App, error) {
-	log := logger.NewDefault(verbose)
-	cfg, err := config.Load()
-	if err != nil {
-		return nil, fmt.Errorf("failed to load config: %v", err)
+	type App struct {
+		Logger    *logger.Logger
+		Config    *config.Config
+		Parser    parser.Parser
+		Templater templater.Templater
+		Bldr   	  builder.Builder
+		Srv	  	  server.Server
 	}
 
-	tmpl, err := templater.NewGoTemplater(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create templater: %v", err)
+	func New(verbose bool) (*App, error) {
+		log := logger.NewDefault(verbose)
+		cfg, err := config.Load()
+		if err != nil {
+			return nil, fmt.Errorf("failed to load config: %v", err)
+		}
+
+		tmpl, err := templater.NewGoTemplater(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create templater: %v", err)
+		}
+
+		prs := parser.NewMarkdownParser()
+
+		return &App{
+			Logger: log,
+			Config: cfg,
+			Templater: tmpl,
+			Parser: prs,
+		}, nil	
 	}
 
-	prs := parser.NewMarkdownParser()
+	func (a *App) Builder() builder.Builder {
+		if a.Bldr == nil {
+			a.Bldr = builder.NewDefault(a.Logger, a.Config, a.Parser, a.Templater)
+		}
+		return a.Bldr
+	}
 
-	return &App{
-		logger: log,
-		config: cfg,
-		templater: tmpl,
-		parser: prs,
-	}, nil	
-}
-
-func (a *App) Builder() builder.Builder {
-	return builder.NewDefault(a.logger, a.config, a.parser, a.templater)
-}
-
-func (a *App) Server(port string) server.Server {
-	return server.NewDefault(a.logger, a.config, port)
-}
+	func (a *App) Server(port string) server.Server {
+		if a.Srv == nil {
+			a.Srv = server.NewDefault(a.Logger, a.Config, port)
+		}
+		return a.Srv
+	}
